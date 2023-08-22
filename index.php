9c167ab15ab2796931c7df9caaed71a236d09fd7
TntTastisch <tnttastisch@gmail.com>
2023-08-23 00:19:18 +0200

    first  site

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/main.css">
+    <title>Chat Webseite</title>
+</head>
+<body>
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <div class="header-center">
+            <a href="#" class="start-link">Start</a>
+        </div>
+        <div class="header-form">
+            <form class="search-form">
+                <input type="text" id="search-input" placeholder="Code eingeben...">
+                <button type="submit">Suchen</button>
+            </form>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="search-container">
+        <h1>Suche nach Codes</h1>
+    </div>
+</main>
+
+<footer>
+    <div class="footer-container">
+        <div class="footer-links">
+            <a href="#">Datenschutz</a> / <a href="#">Cookies</a>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2023 Ihr Unternehmen. Alle Rechte vorbehalten.</p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
